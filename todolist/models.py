4fb95f6aadc68f291c973966aceae7b756837836
Kushagra Gupta <Kushagra@Kushagra.local>
2016-08-07 21:02:49 +0530

    Added Models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(null=False)
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20,null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Task(models.Model):
+    userid = models.ForeignKey(User,null=False)
+    date = models.DateField(null=False)
+    task = models.CharField(max_length=50,null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.task
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['date', 'task', 'userid']
